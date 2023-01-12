e669813c0e792bef7d9d6123acafd5ba50f5dee9
Anastasiia323 <anastasiya.nancy@gmail.com>
2023-01-12 17:53:34 +0200

    Создала программу, где потренировала методы для List

---
+++ lesson_3/programme_for_list_types.py
@@ -0,0 +1,18 @@
+my_first_list = ['song', 'singer', 'guitar']
+
+my_first_list.append('music')
+my_first_list.pop()
+my_first_list.sort(reverse=False)
+my_first_list.reverse()
+my_first_list.clear()
+my_second_list = my_first_list.copy()
+
+
+
+print(my_first_list)
+print(my_second_list)
+
+
+
+
+
