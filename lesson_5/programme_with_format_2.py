fb08dd5fb92d582e16dae5b07b7595d6c366e8ec
Anastasiia323 <anastasiya.nancy@gmail.com>
2023-01-18 20:21:14 +0200

    Создала программу, где вывожу фразу из двух введённых слов с помощью метода .format

---
+++ lesson_5/programme_with_format_2.py
@@ -0,0 +1,8 @@
+word_1 = input('Введите первое слово: ')
+word_2 = input('Введите второе слово: ')
+
+final_phrase = '{} {}'.format(word_1, word_2)
+final_phrase_1 = '!{} ! {}!'.format(word_2, word_1)
+
+print(final_phrase)
+print(final_phrase_1)
