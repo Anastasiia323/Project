91a5be9fc3c7f794a8287f344033f847cbffe847
Anastasiia323 <anastasiya.nancy@gmail.com>
2023-01-19 03:14:39 +0200

    Написала программу для сложения кубов всех целых чисел с использованием цикла while

---
+++ lesson_5/prorgamme_for_number_n_using_while.py
@@ -0,0 +1,23 @@
+n = int(input('Введите число: '))
+i = int in range(1, n+1)
+summa = 0
+
+while i < n:
+    if n != 0:
+        i = i + 1
+        summa = summa + (i ** 3)
+        print(summa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
