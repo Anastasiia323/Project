47281a77f5e3aa7aab755452b97923cd1a0e21ff
Anastasiia323 <anastasiya.nancy@gmail.com>
2023-01-22 02:57:17 +0200

    Протестировала функцию timeit

--- lesson_5/programme_for_timeit.py
+++ lesson_5/programme_for_timeit.py
@@ -1,24 +1,18 @@
 import timeit
-import time
-
-start = time.time()
 
 def write_the_number():
-    a = int(input("Введите число: "))
-    summa = 0
-    for i in range(0, a+1):
-        summa = summa + i
-        print(summa)
+    a = range(2, 10)
+    for i in a:
+        print(i)
 
 write_the_number()
 
-end = time.time()
+print('Время работы функции:', timeit.timeit(stmt='write_the_number()', number=1000, globals=globals()))
 
-print(end)
 
-# практика модуля timeit
 
-print(timeit.timeit('x = sum([1, 2, 3, 4, 5])'))
+
+
 
 
 
