dd1d721930afcc7504441f7d842ef46a562411f1
Anastasiia323 <anastasiya.nancy@gmail.com>
2023-01-23 05:21:26 +0200

    Внесла изменения

--- lesson_6/programme_for_values_and_dicts.py
+++ lesson_6/programme_for_values_and_dicts.py
@@ -1,6 +1,6 @@
 def pet_names(pets):
-    new_pets = {v: k for k, v in pets.items()}
-    return new_pets
+    return {v: k for k, v in pets.items()}
+
 
 pets = {'Cat':'Tom', 'Mouse': 'Jerry', 'Dog': 'Spike'}
 
