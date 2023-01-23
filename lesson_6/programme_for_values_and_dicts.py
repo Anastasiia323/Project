4513965daee8c39d61cae9e8f7bae51433c8aebc
Anastasiia323 <anastasiya.nancy@gmail.com>
2023-01-23 02:05:13 +0200

    Создала функцию для вычисления факториала с помощью рекурсии

--- lesson_6/programme_for_values_and_dicts.py
+++ lesson_6/programme_for_values_and_dicts.py
@@ -2,7 +2,7 @@ def pet_names(pets):
     new_pets = {v: k for k, v in pets.items()}
     return new_pets
 
-pets = {'Cat':'Tom', 'Mouse': 'Jerry', 'Dod': 'Spike'}
+pets = {'Cat':'Tom', 'Mouse': 'Jerry', 'Dog': 'Spike'}
 
 print(pet_names(pets))
 
