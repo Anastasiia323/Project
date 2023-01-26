193c4a7c000e3a1f70dd30907ff529fde1ffe379
Anastasiia323 <anastasiya.nancy@gmail.com>
2023-01-26 18:40:14 +0200

    Создала функцию-декоратор. Функция считает время выполнения другой функции, которая выводит имя владельца и имена животных и их тип

---
+++ lesson_7/programme_for_the_second_dec.py
@@ -0,0 +1,23 @@
+import time
+
+
+def to_dec(func):
+    """Decorating function creating"""
+    def wrapper(owner, **pets):
+        """Wrapper creating"""
+        start = time.time()
+        ret = func(owner, **pets)
+        end = time.time()
+        print(f'Working time: {end-start}')
+        return ret
+    return wrapper
+
+@to_dec
+def main_func(owner, **pets) -> str:
+    time.sleep(0.5)
+    print(f'Owner name: {owner}')
+    for pet, name in pets.items():
+        print(f'{pet}:{name}')
+
+
+main_func('Anastasiia', Cat='Lisa', Dog='Tom', Fish=['Dori', 'Nemo', 'Goldie'], Pig='Piggy', Horse='Pifagor')
