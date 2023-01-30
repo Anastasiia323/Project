490abae594f0c72aaf39d70426cc2e0561c0912a
Anastasiia323 <anastasiya.nancy@gmail.com>
2023-01-30 23:03:01 +0200

    Переписала код

--- lesson_7/programme_for_isdigit_method.py
+++ lesson_7/programme_for_isdigit_method.py
@@ -1,20 +1,33 @@
 from beartype import beartype
 
 @beartype
-def analyze_the_string(text: str) -> float | str:
+def analyze_the_string(my_number: str) -> str:
     """Определяю, содержатся ли во входящей строке цифры
     и, в зависимости от результата, привожу к float"""
-    d = text.isdigit()
-    b = text.replace(',', '.')
-    if d is True:
-        return int(a)
-    elif text[0] == '-':
-        return float(b)
-    elif '.' in text:
-        return float(b)
+    my_number_check = my_number.isdigit()
+    my_number_sym_replace = my_number.replace(',', '.')
+    if '.' in my_number:
+        if my_number[0] != '-':
+            return f'Вы ввели положительное дробное число {float(my_number)}'
+        else:
+            return f'Вы ввели отрицательное дробное число {float(my_number)}'
+
+    if ',' in my_number:
+        my_number_sym_replace = my_number.replace(',', '.')
+        if my_number[0] != '-':
+            return f'Вы вввели положительное дробное число {float(my_number_sym_replace)}'
+        else:
+            return f'Вы ввели отрицательное дробное число {float(my_number_sym_replace)}'
+
+    if my_number_check:
+        return f'Вы ввели положительное целое число {int(my_number)}'
     else:
-        return 'Некорректная строка'
+        if my_number[0] == '-':
+            return f'Вы ввели отрицательное целое число {round(float(my_number))}'
+        else:
+            return 'Вы ввели некорректное число'
 
-a = '-2,4'
 
-print(analyze_the_string(a))
+string_to_exam = '-9.2'
+
+print(analyze_the_string(string_to_exam))
